6ecab27c59ecb53274c193a9eb83718c8708525d
Kat Drobnjakovic <kat.drobnjakovic@shopify.com>
2015-12-09 22:07:25 -0500

    add comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
+  #validation that the title field must be present for the post object to be valid and successfully saved
   validates :title, presence: true
+  #validation that the body field must be present for the post object to be valid and successfully saved
   validates :body, presence: true
 end
